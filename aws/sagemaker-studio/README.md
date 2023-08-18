f393f884bda7ef91ea7ea07fd0c93a0b8ecf59f2
Jeremy Pedersen <jdp@jeremypedersen.com>
2023-08-18 22:12:51 +0800

    Added new script to delete KernelGateway instances

---
+++ aws/sagemaker-studio/README.md
@@ -0,0 +1,7 @@
+# SageMaker Studio Scripts
+
+Scripts to setup and manage SageMaker studio environments (domains), users, and apps. 
+
+## `delete-all-kernel-gateways.py`
+
+This script asks for an AWS region name for input (such as `us-east-1`) and then deletes any and all KernelGateways found in that region. 
---
+++ aws/sagemaker-studio/delete-all-kernel-gateways.py
@@ -0,0 +1,49 @@
+#
+# Author: Jeremy Pedersen
+# Date: 2023-08-18
+# Purpose: delete all KernelGateways in a given region (cleanup)
+#
+import boto3
+
+def delete_all_kernelgateways(region_name):
+    # Initialize SageMaker client for the specified region
+    sagemaker = boto3.client('sagemaker', region_name=region_name)
+
+    # Paginator for listing SageMaker applications
+    paginator = sagemaker.get_paginator('list_apps')
+
+    # Iterate through all SageMaker applications in the region
+    for page in paginator.paginate():
+        for app in page['Apps']:
+            if app['AppType'] == 'KernelGateway':
+
+                # Gather app info
+                domain_name = app['DomainId']
+                try:
+                    user_name = app['UserProfileName']
+                except:
+                    user_name = app['SpaceName']
+                app_type = app['AppType']
+                app_name = app['AppName']
+
+                print('=' * 30)
+                print('Deleting KernelGateway...')
+                print(f'Domain: {domain_name}')
+                print(f'User / Space Name: {user_name}')
+                print(f'App Type: {app_type}')
+                print(f'App Name: {app_name}')
+
+                # Deleting the KernelGateway instance
+                try:
+                    sagemaker.delete_app(
+                        DomainId=domain_name, 
+                        UserProfileName=user_name, 
+                        AppType=app_type, 
+                        AppName=app_name
+                    )
+                except Exception as e:
+                    print(f"Error deleting KernelGateway {app_name}: {e}")
+
+# Ask the user to input a region
+region = input('Enter an AWS region name (such as us-east-1): ').strip()
+delete_all_kernelgateways(region)
